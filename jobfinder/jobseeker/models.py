50295ba82b7abf3780f430032486b23181b9d142
vikrampatel0408 <96529637+vikrampatel0408@users.noreply.github.com>
2023-02-13 15:02:06 +0530

    Update models.py

--- jobfinder/jobseeker/models.py
+++ jobfinder/jobseeker/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class jobseeker(models.Model):
     name =models.CharField(max_length=255)
-    email=models.models.CharField(max_length=255)
+    email=models.CharField(max_length=255)
     phone=models.CharField(max_length=255)
     city=models.CharField(max_length=255)
     state=models.CharField(max_length=255)
