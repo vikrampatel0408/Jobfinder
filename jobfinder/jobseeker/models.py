be618b6ab62cb79a8702a47fedfc403bfb1c051f
vikrampatel0408 <96529637+vikrampatel0408@users.noreply.github.com>
2023-02-13 14:59:58 +0530

    Add files via upload

---
+++ jobfinder/jobseeker/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class jobseeker(models.Model):
+    name =models.CharField(max_length=255)
+    email=models.models.CharField(max_length=255)
+    phone=models.CharField(max_length=255)
+    city=models.CharField(max_length=255)
+    state=models.CharField(max_length=255)
+    password=models.CharField(max_length=255)
+
+    
